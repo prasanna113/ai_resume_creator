e962143fa0d72bdc2d2ea7d267c5eab2685195d9
Prasanna Deshmukh <prasannadeshmukh@Prasannas-MacBook-Air.local>
2025-03-23 10:17:11 +0530

    Using doppler to fetch secrets

--- config.py
+++ config.py
@@ -1,20 +1,24 @@
 import os
 from dotenv import load_dotenv
-import secrets
 
 # Load environment variables from a .env file if available
 load_dotenv()
 
+# DB Password
+POSTGRE_PASSWORD = os.getenv("POSTGRE_PASSWORD")
+
 # Database configuration
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:wipDr^Isemi917@localhost:1520/ai_resume_db")
+DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://postgres:{POSTGRE_PASSWORD}@localhost:1520/ai_resume_db")
 
 # JWT configuration
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", 'a5282bbcfd35a93fe40825f82f4c6db1ea02644d7cf9943b05dbc9b698b633be')
 JWT_ALGORITHM = "HS256"
 
+# OpenAI API configuration
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Stripe configuration
 STRIPE_SECRET = os.getenv("STRIPE_SECRET", "your_stripe_secret_key")
 
 EMAIL_SENDER = os.getenv("EMAIL_SENDER", "airesumecreator@gmail.com")
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "omwy tblk xshq qjvb")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
